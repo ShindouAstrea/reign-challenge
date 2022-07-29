fe216427f27cdaa0a22a94e6909fe2bff88ba62e
Jose Silva <jose.silva.a@mail.pucv.cl>
2022-07-29 03:29:18 -0400

    Cambios de navegacion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import './css/Buttons.css'
+import SelectorFav from './components/SelectorFav';
+import {Button} from 'react-bootstrap';
+import {BrowserRouter as Router,Routes,Route,Link} from 'react-router-dom';
+import AllPost from './pages/AllPost';
+import FavPost from './pages/FavPost';
+import {Pagination} from '@mui/material';
+import React, { useEffect,useState } from 'react';
+
 function App() {
 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <h1>Hola Mundo</h1>
-        </div>
-          
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Front-End-Test---Home-view">
+      <div className="Rectangle-2-Copy">
+        <span className="HACKER-NEWS Text-Style">HACKER NEWS</span>
+      </div>
+      
+      
+        <Router>
+          <SelectorFav/>
+          <div className="ContainerPosts">
+        {/* Links for all the routes */}
+            <Routes>
+              <Route path="All" element={<AllPost />}/>
+              <Route path="Faves" element={<FavPost/>}/>
+            </Routes>
+          </div>
+        </Router>
     </div>
   );
 }
