9b3dbef8f59d425c9610cf69ecbf8e6dad15e87a
Jsilva_dev <jose.silva.a@mail.pucv.cl>
2022-07-30 23:17:38 -0400

    New Name and changes about structure

---
+++ src/components/SelectorDropDown.js
@@ -0,0 +1,17 @@
+
+import React from "react";
+import '../css/DropDown.css' ;
+
+
+export default function SelectorDropDown(){
+    
+    return(
+        <>
+            <div className="Rectangle-26-Copy-23">
+                <div className="Rectangle">
+                    <div  className="Mask"></div>
+                </div>
+            </div>
+        </>
+    );
+}
