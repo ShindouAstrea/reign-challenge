d39cfde5dacaf7b6c4abe620987882dd56035304
Jsilva_dev <jose.silva.a@mail.pucv.cl>
2022-07-30 23:38:56 -0400

    Component move it

--- src/pages/FavPost.js
+++ src/pages/FavPost.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from   'react';
 import CardPost from '../components/CardPost';
 import Pagination from '@mui/material/Pagination';
+import {Row,Col } from 'react-bootstrap' ;
+import SelectorDropDown from '../components/SelectorDropDown';
+
 import '../css/CardPost.css' ;
 
 export default function FavPost(){
-  const [url,setUrl]=useState(`https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=0`);
+  const [url,setUrl]=useState(`https://hn.algolia.com/api/v1/search_by_date?page=0`);
   const[arrayPost,setPost]=useState([]);
   const [numberPages,setNumber]=useState();
   const [page, setPage] = React.useState(1);
   
   const handleChange = (event, value) => {
     setPage(value);
-    setUrl(`https://hn.algolia.com/api/v1/search_by_date?query=reactjs&page=${value}`)
+    setUrl(`https://hn.algolia.com/api/v1/search_by_date?page=${value}`)
   };
     const getPosts =(url)=>{
       fetch(url)
@@ -23,20 +26,18 @@ export default function FavPost(){
       getPosts(url);
     },[url])
     return(
-      
-      <div className="row">
-        {arrayPost.map((item,index)=>{
-          return(
-            <>
-            <div key={index}>
-              <CardPost item={item}/>
-              
-            </div>
-    
-            </>
-          )
-        })}
-        <Pagination count={numberPages} page={page} onChange={handleChange}/>
-      </div>   
+      <Row>
+        <SelectorDropDown/>
+        <Row>
+          {
+            arrayPost.map((item,index)=>(
+              <Col key={index}>
+                <CardPost item={item}/>
+              </Col>
+            ))
+          }
+        </Row>
+      <Pagination count={numberPages} page={page} onChange={handleChange}/>
+    </Row>   
     );
 }
